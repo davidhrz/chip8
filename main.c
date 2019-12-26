439bdfcb1d414fea71c22beaf6d7146780103ea5
David H <david.horozian@epita.fr>
2019-12-26 19:05:34 +0100

    initial

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void)
+{
+    uint8_t mem[4096];
+    uint8_t V0;
+    uint8_t V1;
+    uint8_t V2;
+    uint8_t V3;
+    uint8_t V4;
+    uint8_t V5;
+    uint8_t V6;
+    uint8_t V7;
+    uint8_t V8;
+    uint8_t V9;
+    uint8_t VA;
+    uint8_t VB;
+    uint8_t VC;
+    uint8_t VD;
+    uint8_t VE;
+    uint8_t VF;
+    uint16_t I;
+
+    uint16_t PC = 0x200;
+    uint8_t SP;
+
+    for (;;)
+    {
+
+    }
+
+    return EXIT_SUCCESS;
+}
